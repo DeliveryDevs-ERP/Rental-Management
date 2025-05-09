a6d8589f3b058a5f4d10ae4200e2c39215af0e00
OsamaASidd <osama.siddiqui2017@gmail.com>
2025-05-09 18:16:08 +0500

    bug fix

--- rental_management/rental_management/doctype/vehicle_movement/vehicle_movement.js
+++ rental_management/rental_management/doctype/vehicle_movement/vehicle_movement.js
@@ -2,6 +2,28 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Vehicle Movement", {
+
+    vehicle(frm) {
+        if (frm.doc.vehicle) {
+            frappe.call({
+                method: "frappe.client.get_value",
+                args: {
+                    doctype: "Vehicle",
+                    filters: { name: frm.doc.vehicle },
+                    fieldname: "custom_project"
+                },
+                callback: function (r) {
+                    if (r.message) {
+                        frm.set_value("project_id", r.message.custom_project);
+                    }
+                }
+            });
+        }
+        else{
+            frm.set_value("project_id", null);
+        }
+    },
+
     status(frm) {
         if (frm.doc.status) {
             frm.set_query("vehicle", () => {
