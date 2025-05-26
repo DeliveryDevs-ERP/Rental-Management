a3db2d08217ecd284c8eedf44051b60f5dbcd363
OsamaASidd <osama.siddiqui2017@gmail.com>
2025-05-26 16:01:39 +0500

    CICPA print formats

--- rental_management/rental_management/doctype/loa/loa.js
+++ rental_management/rental_management/doctype/loa/loa.js
@@ -1,12 +1,23 @@
 // Copyright (c) 2025, osama.ahmed@deliverydevs.com and contributors
 // For license information, please see license.txt
 
+// Copyright (c) 2025, osama.ahmed@deliverydevs.com and contributors
+// For license information, please see license.txt
+
 frappe.ui.form.on("LOA", {
     refresh: function(frm) {
         if (!frm.is_new() && frm.doc.docstatus === 1) { 
-            frm.add_custom_button(__('CICPA'), function() {
+            frm.add_custom_button(__('Vehicle CICPA'), function() {
                 frappe.new_doc("CICPA", {
-                    loa: frm.doc.name
+                    loa: frm.doc.name,
+                    cicpa_type: "Vehicle"
+                });
+            }, __("Create"));  
+
+            frm.add_custom_button(__('Driver CICPA'), function() {
+                frappe.new_doc("CICPA", {
+                    loa: frm.doc.name,
+                    cicpa_type: "Driver"
                 });
             }, __("Create"));  
         }
